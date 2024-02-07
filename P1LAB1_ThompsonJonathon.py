deeac8c2e3f22f3088472b94e4097489dd633585
JTatFTCC <159356168+JTatFTCC@users.noreply.github.com>
2024-02-07 16:04:20 -0500

    Add files via upload

---
+++ P1LAB1_ThompsonJonathon.py
@@ -0,0 +1,11 @@
+# Jonathon Thompson
+# 2/7/24
+# P1LAB1
+# For this project I have to build code that displays a welcome message depending on the name inputed.
+
+print('Your first name:', end ='')
+first_name = input()
+
+print('Hey', first_name)
+print('Welcome to zybooks!')
+
